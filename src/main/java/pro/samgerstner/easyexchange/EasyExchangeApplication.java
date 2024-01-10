744c9e53a7007754f40a88dfed20036cc3787251
Sam Gerstner <Sam.Gerstner@samgerstner.pro>
2024-01-10 13:24:57 -0700

    Basic Project Structure
    Signed-off-by: Sam Gerstner <Sam.Gerstner@samgerstner.pro>

---
+++ src/main/java/pro/samgerstner/easyexchange/EasyExchangeApplication.java
@@ -0,0 +1,16 @@
+package pro.samgerstner.easyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExchangeApplication
+{
+
+   public static void main(String[] args)
+   {
+
+      SpringApplication.run(EasyExchangeApplication.class, args);
+   }
+
+}
---
+++ src/test/java/pro/samgerstner/easyexchange/EasyExchangeApplicationTests.java
@@ -0,0 +1,16 @@
+package pro.samgerstner.easyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyExchangeApplicationTests
+{
+
+   @Test
+   void contextLoads()
+   {
+
+   }
+
+}
